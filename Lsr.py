3db0290ff8640581c6a1fbf4e97c7d74f139a61b
Edwin Kurniawan <kurniawanedwin95@gmail.com>
2016-10-26 03:04:57 +1100

    multithread properly close, use of enumerate list

--- Lsr.py
+++ Lsr.py
@@ -6,12 +6,19 @@ import sys
 from threading import Thread
 import time
 
-def broadcast(s, packet, num_of_neighbour, broadcast_port):
+def broadcast(s, packet, num_of_neighbour, neighbours_port):
     while True:
-        for i in range(0,num_of_neighbour):
-            s.sendto(packet, ('127.0.0.1',broadcast_port[i]))
-            print "sent to %d" %(broadcast_port[i])
+        for i, v in enumerate(neighbours_port):
+            s.sendto(packet, ('127.0.0.1',neighbours_port[v]))
+            print 'sent to %s' %(v)
         time.sleep(1)
+
+def rebroadcast(s, packet, num_of_neighbour, broadcast_port):
+    #gotta checks for incoming packet, and will rebroadcast while also adding more to the topology
+    message, client = s.recvfrom() #will check client's port and match it with existing
+    for i in range(0,num_of_neighbour):
+        s.sendto(packet, ('127.0.0.1',broadcast_port[i]))
+        print 'rebroadcasted packet from'
 
 def testprint():
     print 'test'
@@ -26,25 +33,26 @@ neighbours_port = {}
 threads = []
 num_of_neighbour = f.readline()
 num_of_neighbour = int(num_of_neighbour)
-broadcast_port = []
 for i in range (0,num_of_neighbour):
   read = f.readline()
   read = read.split()
   neighbours_cost[read[0]] = float(read[1])
   neighbours_port[read[0]] = int(read[2])
-  broadcast_port.append(neighbours_port[read[0]])
 
 cur_time = time.time() #in seconds
 print cur_time
-t1 = Thread(target=testprint)
-packet = 'something'
-t2 = Thread(target=broadcast, kwargs={'s':s,'packet':packet,'num_of_neighbour':num_of_neighbour,'broadcast_port':broadcast_port})
-threads.append(t1)
-threads.append(t2)
-t1.start()
-t2.start()
 
-# while True:
-#   if (time.time()-curtime) > 1000
-#     broadcast(s, packet, num_of_neighbour, broadcast_port)
-#1 sec send, 30 sec print
+try:
+    t1 = Thread(target=testprint)
+    packet = 'something'
+    t2 = Thread(target=broadcast, kwargs={'s':s,'packet':packet,'num_of_neighbour':num_of_neighbour,'neighbours_port':neighbours_port})
+    t2.daemon = True
+    threads.append(t1)
+    threads.append(t2)
+    t1.start()
+    t2.start()
+    while True:
+        i = 1
+except KeyboardInterrupt:
+    print 'keyboard interrupt triggered'
+    sys.exit(1)
